a8970e694db63061f1499e6ae736910b43b4af36
guilhermecantonsfa <guilherme.canton@gruposfa.com.br>
2021-10-28 16:17:41 -0300

    Atualizacoes

--- src/main/java/corp/formulacerta/integracao/security/SecurityConstants.java
+++ src/main/java/corp/formulacerta/integracao/security/SecurityConstants.java
@@ -6,7 +6,7 @@ public class SecurityConstants {
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/api/users/sign-up";
-	public static final String PUBLIC_URL = "/public/**";
+	public static final String PUBLIC_URL = "/api/public/**";
 	public static final String CLAIM_PERMISSOES = "permissoes";
 	public static final String CLAIM_ID_USER = "id";
 	public static final String CLAIM_USER_LOGADO = "nome";
